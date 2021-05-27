f50e5152af588bffcf53a4bb0e0d79c2a3f9782f
Kate Doxey <kedoxey@gmail.com>
2021-05-27 16:54:47 -0700

    added noise stimulus and temperature, output of stimulation

--- KonstantoudakiEtAl2014/experiment/setup_net.py
+++ KonstantoudakiEtAl2014/experiment/setup_net.py
@@ -1,6 +1,4 @@
 from netpyne import specs, sim
-import random
-from itertools import repeat
 
 # weight params
 ampaweightpr = 0.00008
@@ -86,24 +84,6 @@ netParams.synMechParams['NMDAIN'] = {'mod': 'NMDAIN'}
 netParams.synMechParams['GABAB'] = {'mod': 'GABAb'}
 
 netParams.defaultThreshold = -20
-
-# autapses
-# ampa random seed = 3, nmda random seed = 124
-# netParams.connParams['autapses'] = {
-#         'preConds': {'pop': 'PYR_pop'},
-#         'postConds': {'pop': 'PYR_pop'},
-#         'sec': 'dend_0',
-#         'synMech': ['AMPA', 'NMDA'],
-#         # 'weight': list(repeat([list(repeat(ampaweight, automaxsyn)), list(repeat(nmdaweight, automaxsyn))], 16)),
-#         'weight': list(repeat([ampaweight, nmdaweight], 16)),
-#         'synsPerConn': automaxsyn,
-#         # 'delay': list(repeat([list(repeat(random.gauss(0.96, 0.11), automaxsyn)),
-#         #                         list(repeat(random.gauss(1.33, 0.13), automaxsyn))], 16)),
-#         'delay': list(repeat(['normal(0.96, 0.11)', 'normal(1.33, 0.13)'], 16)),
-#         # 'delay': list(repeat([ampa_delay, nmda_delay], 16)),
-#         'connList': [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9],
-#                      [10, 10], [11, 11], [12, 12], [13, 13], [14, 14], [15, 15]]
-# }
 
 # -------------------- PC-PC AMPA --------------------
 netParams.connParams['PYR->PYR-AMPA'] = {
@@ -275,9 +255,26 @@ netParams.connParams['ISin->PYR'] = {
         'delay': 'normal(1.8, 0.8)'
 }
 
+for gid in range(0, 16):
+        netParams.stimSourceParams[f'noise_source-{gid}'] = {
+                'type': 'NetStim',
+                'rate': 100,
+                'start': 0,
+                'noise': 1
+        }
+        netParams.stimTargetParams[f'noise->{gid}'] = {
+                'source': f'noise_source-{gid}',
+                'conds': {'cellList': [gid]},
+                'synMech': 'AMPA',
+                'weight': ampaweightpr,
+                'synsPerConn': 1,
+                'delay': 5,
+                'sec': 'soma',
+                'loc': 'uniform(0, 1)'
+        }
+
 netParams.stimSourceParams['ns1'] = {
         'type': 'NetStim',
-        # 'rate': 100,
         'interval': 50,
         'number': 10,
         'start': 0,
@@ -303,8 +300,9 @@ netParams.stimTargetParams['nc2'] = {
         'sec': 'dend_1',
         'loc': 'uniform(0,1)'
 }
-# simConfig.hParams['celsius'] = 5
-# simConfig.allowSelfConns = True
+
+simConfig.hParams['celsius'] = 34
+simConfig.allowSelfConns = False
 simConfig.duration = 1000
 simConfig.dt = 0.025
 simConfig.verbose = False
@@ -313,5 +311,6 @@ simConfig.recordStep = 1
 simConfig.savePickle = False
 simConfig.analysis['plotRaster'] = {'orderInverse': True, 'saveFig': 'output/raster.png'}
 simConfig.analysis['plotTraces'] = {'include': ['PYR_pop', 'FSin_pop', 'RSin_pop', 'ISin_pop'], 'saveFig': 'output/traces.png'}
+simConfig.analysis['plotConn'] = {'groupBy': 'cell', 'feature': 'numConns', 'saveFig': 'output/conns.png'}
 
 sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
